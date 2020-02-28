2a6832b03c225c5d4dfdd3e4b96ce0204221106c
mcmainsliam <liam@mcmains.net>
2020-02-28 17:18:50 -0600

    Update css

--- invoiceExample/postConfiguration.js
+++ invoiceExample/postConfiguration.js
@@ -3,17 +3,20 @@ const dataSchema = require("./dataSchema");
 module.exports = {
   stylesheet: {
     id: "stylesheet_id",
-    url: ""
+    url:
+      "https://raw.githubusercontent.com/McMainsLiam/datas/master/invoiceExample/example.css"
   },
   handlebarsFile: {
     id: "handlebars_id",
-    url: "",
+    url:
+      "https://raw.githubusercontent.com/McMainsLiam/datas/master/invoiceExample/example.handlebars",
     jsonSchema: dataSchema
   },
   contexts: [
     {
       id: "invoiceDataContext",
-      url: ""
+      url:
+        "https://raw.githubusercontent.com/McMainsLiam/datas/master/invoiceExample/data1.json"
     }
   ]
 };
